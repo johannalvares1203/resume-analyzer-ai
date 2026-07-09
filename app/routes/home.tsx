51b30ff4dc7ee149d52062dbbbc0090782e8b037
Johann Alvares <johannalvares2003@gmail.com>
2026-07-09 22:18:16 +0530

    Components, libs and other files added.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import { resume } from "react-dom/server";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import { useState } from "react";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Optimizer" },
+    { name: "description", content: "Smart Feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const [resumes, setResumes] = useState<Resume[]>([]);
+
+  
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+
+      </section>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
 }
