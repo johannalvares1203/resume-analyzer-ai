602bd7ee8c6b2cded54882d8b949f847c4a2d9d5
Johann Alvares <johannalvares2003@gmail.com>
2026-07-13 13:08:54 +0530

    Added cloud storage, PDF-Image Conversion, Key-Value Storage and AI feedback

--- types/puter.d.ts
+++ types/puter.d.ts
@@ -11,59 +11,59 @@ interface FSItem {
     accessed: number;
     size: number | null;
     writable: boolean;
-  }
-  
-  interface PuterUser {
+}
+
+interface PuterUser {
     uuid: string;
     username: string;
-  }
-  
-  interface KVItem {
+}
+
+interface KVItem {
     key: string;
     value: string;
-  }
-  
-  interface ChatMessageContent {
+}
+
+interface ChatMessageContent {
     type: "file" | "text";
     puter_path?: string;
     text?: string;
-  }
-  
-  interface ChatMessage {
+}
+
+interface ChatMessage {
     role: "user" | "assistant" | "system";
     content: string | ChatMessageContent[];
-  }
-  
-  interface PuterChatOptions {
+}
+
+interface PuterChatOptions {
     model?: string;
     stream?: boolean;
     max_tokens?: number;
     temperature?: number;
     tools?: {
-      type: "function";
-      function: {
-        name: string;
-        description: string;
-        parameters: { type: string; properties: {} };
-      }[];
+        type: "function";
+        function: {
+            name: string;
+            description: string;
+            parameters: { type: string; properties: {} };
+        }[];
     };
-  }
-  
-  interface AIResponse {
+}
+
+interface AIResponse {
     index: number;
     message: {
-      role: string;
-      content: string | any[];
-      refusal: null | string;
-      annotations: any[];
+        role: string;
+        content: string | any[];
+        refusal: null | string;
+        annotations: any[];
     };
     logprobs: null | any;
     finish_reason: string;
     usage: {
-      type: string;
-      model: string;
-      amount: number;
-      cost: number;
+        type: string;
+        model: string;
+        amount: number;
+        cost: number;
     }[];
     via_ai_chat_service: boolean;
-  }
+}
